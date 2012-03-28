4920c0e42d337275d5e0775eb26b6a849855ebb5
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2012-03-29 00:55:33 +0800

    Exercise 2.2

---
+++ public/javascripts/Chapter2.js
@@ -0,0 +1,8 @@
+function twoPowTen() {
+  var result = 1, count = 0;
+  while (count < 10) {
+    result *= 2;
+    count += 1;
+  }
+  return result;
+}
---
+++ spec/javascripts/Chapter2Spec.js
@@ -0,0 +1,5 @@
+describe("twoPowTen", function() {
+  it("returns 2 to the power of 10", function() {
+    expect(twoPowTen()).toEqual(1024);
+  });
+});
